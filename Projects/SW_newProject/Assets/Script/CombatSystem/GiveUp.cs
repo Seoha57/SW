2a203fdb7b3781b8356c2fe44c81e8b30b50e469
Wonjae Jung <wonjae0925@gmail.com>
2019-12-06 13:55:36 -0800

    quit in game

--- Projects/SW_newProject/Assets/Script/CombatSystem/GiveUp.cs
+++ Projects/SW_newProject/Assets/Script/CombatSystem/GiveUp.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GiveUp : MonoBehaviour
 {
     public Button m_button;
+    public Button cancelButton;
     public Image m_background;
     public Text m_text;
     // Start is called before the first frame update
@@ -13,6 +14,7 @@ public class GiveUp : MonoBehaviour
     {
         m_button.gameObject.SetActive(false);
         m_background.gameObject.SetActive(false);
+        cancelButton.gameObject.SetActive(false);
         m_text.gameObject.SetActive(false);
     }
 
@@ -25,6 +27,7 @@ public class GiveUp : MonoBehaviour
             {
                 m_background.gameObject.SetActive(true);
                 m_button.gameObject.SetActive(true);
+                cancelButton.gameObject.SetActive(true);
                 m_text.gameObject.SetActive(true);
                 Time.timeScale = 0;
             }
@@ -32,9 +35,19 @@ public class GiveUp : MonoBehaviour
             {
                 m_background.gameObject.SetActive(false);
                 m_button.gameObject.SetActive(false);
+                cancelButton.gameObject.SetActive(false);
                 m_text.gameObject.SetActive(false);
                 Time.timeScale = 1.0f;
             }
         }
+    }
+
+    public void Cancel()
+    {
+        m_background.gameObject.SetActive(false);
+        m_button.gameObject.SetActive(false);
+        cancelButton.gameObject.SetActive(false);
+        m_text.gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
